4b9f9e9b0f6f45277e060839dd32e7785f955527
José Pedro Martinez <brightjotaxiv@gmail.com>
2020-01-21 15:21:23 -0300

    UPD: refactored human pose estimation pipeline

--- meerkat_demo.py
+++ meerkat_demo.py
@@ -33,6 +33,61 @@ import cv2
 
 import opt
 
+def dets_from_image(det_model, args, img, dims):
+    inp_dim = int(args['inp_dim'])
+    confidence = args['confidence']
+    num_classes = args['num_classes']
+    nms_thresh = args['nms_thesh']
+
+    prediction = det_model(img, CUDA=True)
+
+    dets = dynamic_write_results(prediction, confidence, num_classes, nms=True, nms_conf=nms_thresh)
+
+    if isinstance(dets, int) or dets.shape[0] == 0:
+        return None, None
+
+    dets = dets.cpu()
+
+    dims = torch.index_select(dims, 0, dets[:, 0].long())
+    scaling_factor = torch.min(inp_dim / dims, 1)[0].view(-1, 1)
+
+    # coordinate transfer
+    dets[:, [1, 3]] -= (inp_dim - scaling_factor * dims[:, 0].view(-1, 1)) / 2
+    dets[:, [2, 4]] -= (inp_dim - scaling_factor * dims[:, 1].view(-1, 1)) / 2
+
+    
+    dets[:, 1:5] /= scaling_factor
+    for j in range(dets.shape[0]):
+        dets[j, [1, 3]] = torch.clamp(dets[j, [1, 3]], 0.0, dims[j, 0])
+        dets[j, [2, 4]] = torch.clamp(dets[j, [2, 4]], 0.0, dims[j, 1])
+    boxes = dets[:, 1:5]
+    scores = dets[:, 5:6]
+
+    return boxes, scores
+
+def pose_from_dets(pose_model, args, boxes, scores, orig_img):
+    inps = torch.zeros(boxes.size(0), 3, args['inputResH'], args['inputResW'])
+    pt1 = torch.zeros(boxes.size(0), 2)
+    pt2 = torch.zeros(boxes.size(0), 2)
+
+    inp = im_to_torch(cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB))
+    inps, pt1, pt2 = crop_from_dets(inp, boxes, inps, pt1, pt2)
+
+    hm = pose_model(inps.cuda())
+    hm = hm.cpu()#.data()
+
+    if args['matching']:
+        preds = getMultiPeakPrediction(
+            hm, pt1.numpy(), pt2.numpy(), args['inputResH'], args['inputResW'], args['outputResH'], args['outputResW'])
+        result = matching(boxes, scores.numpy(), preds)
+    else:
+        preds_hm, preds_img, preds_scores = getPrediction(
+            hm, pt1, pt2, args['inputResH'], args['inputResW'], args['outputResH'], args['outputResW'])
+        result = pose_nms(
+            boxes, scores, preds_img, preds_scores)
+
+    return result
+
 def main():
     ''' arg parsing '''
     args = vars(opt.parser.parse_args())
@@ -98,47 +153,12 @@ def main():
 
             ''' human detection '''
             img = img.cuda()
-            prediction = det_model(img, CUDA=True)
 
-            dets = dynamic_write_results(prediction, confidence, num_classes, nms=True, nms_conf=nms_thresh)
-
-            if isinstance(dets, int) or dets.shape[0] == 0:
+            boxes, scores = dets_from_image(det_model, args, img, dims)
+            if boxes is None or scores is None:
                 continue
-
-            dets = dets.cpu()
-
-            dims = torch.index_select(dims, 0, dets[:, 0].long())
-            scaling_factor = torch.min(inp_dim / dims, 1)[0].view(-1, 1)
-
-            # coordinate transfer
-            dets[:, [1, 3]] -= (inp_dim - scaling_factor * dims[:, 0].view(-1, 1)) / 2
-            dets[:, [2, 4]] -= (inp_dim - scaling_factor * dims[:, 1].view(-1, 1)) / 2
-
-            
-            dets[:, 1:5] /= scaling_factor
-            for j in range(dets.shape[0]):
-                dets[j, [1, 3]] = torch.clamp(dets[j, [1, 3]], 0.0, dims[j, 0])
-                dets[j, [2, 4]] = torch.clamp(dets[j, [2, 4]], 0.0, dims[j, 1])
-            boxes = dets[:, 1:5]
-            scores = dets[:, 5:6]
 
             #print(dets[:,0]) # that's the batch index
-            
-            cv2.imwrite(os.path.join(args['outputpath'], 'frame_%d_input.jpg'%frame_idx), orig_img)
-
-            dets_img = orig_img.copy()
-            for box in boxes:
-                dets_img = cv2.rectangle(dets_img, tuple(box[:2]), tuple(box[2:]), (255, 255, 255))
-            
-            cv2.imwrite(os.path.join(args['outputpath'], 'frame_%d_dets.jpg'%frame_idx), dets_img)
-
-            if isinstance(boxes, int) or boxes.shape[0] == 0:
-                continue
-
-            inps = torch.zeros(boxes.size(0), 3, args['inputResH'], args['inputResW'])
-            pt1 = torch.zeros(boxes.size(0), 2)
-            pt2 = torch.zeros(boxes.size(0), 2)
-
             
             # self.Q.put((orig_img[k], im_name[k], boxes, scores[dets[:,0]==k], inps, pt1, pt2))               
             # end of DetectionLoader.update
@@ -148,38 +168,31 @@ def main():
             if boxes is None or boxes.nelement() == 0:
                 continue
 
-            inp = im_to_torch(cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB))
-            inps, pt1, pt2 = crop_from_dets(inp, boxes, inps, pt1, pt2)
-
             # self.Q.put((inps, orig_img, im_name, boxes, scores, pt1, pt2))
             # end of DetectionProcessor.update
             # beggining of video_demo.py main loop (more specifically line 75)
 
             ''' pose estimation '''
-            hm = pose_model(inps.cuda())
-            hm = hm.cpu()#.data()
-            #print(frame_idx, hm)
-
             # end of video_demo.py main loop
             # beggining of DataWriter.save (which is redirected to DataWriter.update)
 
-            if args['matching']:
-                preds = getMultiPeakPrediction(
-                    hm, pt1.numpy(), pt2.numpy(), args['inputResH'], args['inputResW'], args['outputResH'], args['outputResW'])
-                result = matching(boxes, scores.numpy(), preds)
-            else:
-                preds_hm, preds_img, preds_scores = getPrediction(
-                    hm, pt1, pt2, args['inputResH'], args['inputResW'], args['outputResH'], args['outputResW'])
-                result = pose_nms(
-                    boxes, scores, preds_img, preds_scores)
+            result = pose_from_dets(pose_model, args, boxes, scores, orig_img)
 
             tf = time.time()
             total_time = tf-t0
             print("[%d] Computed %d results in %f seconds (%f FPS)" % (frame_idx, len(result), total_time, (1/float(total_time))))
 
-            frame_with_joints = vis_frame_fast(orig_img, {'imgname': "%d" % frame_idx, 'result': result}, format='coco')
 
-            cv2.imwrite(os.path.join(args['outputpath'], 'frame_%d_joints.jpg'%frame_idx), frame_with_joints)
+            ''' output images '''
+            cv2.imwrite(os.path.join(args['outputpath'], 'input_%d.jpg'%frame_idx), orig_img)
+
+            dets_img = orig_img.copy()
+            for box in boxes:
+                dets_img = cv2.rectangle(dets_img, tuple(box[:2]), tuple(box[2:]), (255, 255, 255))
+            cv2.imwrite(os.path.join(args['outputpath'], 'dets_%d.jpg'%frame_idx), dets_img)
+
+            frame_with_joints = vis_frame_fast(orig_img, {'imgname': "%d" % frame_idx, 'result': result}, format='coco')
+            cv2.imwrite(os.path.join(args['outputpath'], 'joints_%d.jpg'%frame_idx), frame_with_joints)
 
                 
         frame_idx += 1
